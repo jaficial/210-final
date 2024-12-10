82d5e5764a536a6bd14ff9a2040ff8914a5311b9
Jed Aficial's PC <jedaficial1@gmail.com>
2024-12-09 18:45:19 -0800

    Initial commit

--- main.cpp
+++ main.cpp
@@ -1,3 +1,39 @@
 // COMSC-210 | Jed Aficial | Final
 // github link: https://github.com/jaficial/210-final
 
+#include <iostream>
+#include <fstream>
+#include <deque>
+#include <vector>
+
+using namespace std;
+
+string names[] = {"Ella", "Inga", "Zane", "Oleg", "Wade", "Yale", "Tami", 
+                    ""}
+
+// NOTE: 4 BOOTHS TO CREATE:
+/*
+    COFFEE BOOTH: USE LINKED LIST
+    - linked list struct should be a node that contains the customer's name and drink order
+    - create data arrays with names and drink orders from which to randomly create customer data
+    - run for 10 rounds
+    - initialize queue with 3 customers
+        - for any round, there's a 50% probability that someone will join the queue
+        - customer at head of queue is always served
+    - sometimes there might not be a customer being served 
+
+*/ 
+
+struct Node {
+    string customer_name;
+    string drink_order;
+};
+
+int main(){
+    ifstream fin("names.txt");
+    srand(time(0));
+
+
+    fin.close();
+    return 0;
+}
